06a40c87583562bf5ea67018b9ac5eed26638818
Umair Shaikh <imumairshaikh07@gmail.com>
2025-05-03 10:56:34 +0530

    made some changes

--- controller/payment.js
+++ controller/payment.js
@@ -25,8 +25,8 @@ export const order = async (req, res) => {
             payment_method_types: ["card"],
             line_items,
             mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://nature-nook-seven.vercel.app/success",
+            cancel_url: "https://nature-nook-seven.vercel.app/cancel",
         });
 
         res.json({ id: session.id });
