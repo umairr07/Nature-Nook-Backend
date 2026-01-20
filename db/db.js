9b5adc41f04f3fe06eeb9fb4d9ac39100d371393
Umair Shaikh <imumairshaikh07@gmail.com>
2026-01-20 22:26:44 +0530

    made some changes

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
+        //mongo url
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
